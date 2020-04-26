a7f58b70fd865a0a355195916da5584e5460f803
Madhav Paudel <poudelmadhav143@gmail.com>
2020-04-26 12:44:04 +0545

    design of message card app completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,94 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Message Card App"),
-        ),
-        body: Center(
-          child: TextField(
-            maxLength: 30,
-            autofocus: true,
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: "Your message",
-              helperText: "You can write your message here",
-              border: OutlineInputBorder(),
-              prefixIcon: Icon(Icons.edit),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String message = "OK";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Message Card App"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Container(
+              height: 260.0,
+              color: Colors.red,
+              child: Center(
+                child: Text(
+                  message,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontFamily: "cursive",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              maxLength: 30,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Your message",
+                helperText: "You can write your message here",
+                prefixIcon: Icon(
+                  Icons.edit,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: RaisedButton(
+              onPressed: () {},
+              color: Colors.red,
+              child: Text(
+                "Clear the text",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Card(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.red,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.blue,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                  ),
+                  CircleAvatar(
+                    backgroundColor: Colors.orange,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
